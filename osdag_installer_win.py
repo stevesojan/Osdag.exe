3270acf2cfb06c9f05687ec78745c59aef3a7167
Steve <stevesojan9@gmail.com>
2024-12-22 15:12:36 +0530

    Corrected Programmatic Installation of Miniconda

--- osdag_installer_win.py
+++ osdag_installer_win.py
@@ -11,24 +11,25 @@ import sys
 import requests
 
 def install_miniconda():
-    if not os.path.exists("C:\\Miniconda3") or not os.path.exists("%USERPROFILE%\Miniconda3"):
+    upf = os.environ.get('USERPROFILE')
+    if os.path.exists(r"C:\miniconda3") == False and os.path.exists(f"{upf}\\miniconda3") == False:
+        # os.path.exists(r"C:\miniconda3") == False - checks if miniconda3 is not installed system wide for all users in C drive 
         installer_url = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
         installer_name = "Miniconda3-latest-Windows-x86_64.exe"
     
     # Check if the installer is already downloaded
-        if not os.path.exists(installer_name):
-            print("Downloading the latest Miniconda installer...")
-            response = requests.get(installer_url, stream=True)
-            with open(installer_name, "wb") as file:
-                for chunk in response.iter_content(chunk_size=1024):
-                    if chunk:  # Filter out keep-alive chunks
-                        file.write(chunk)
-                        print("Download completed.")
-        else:
-            print("Installer already exists. Skipping download.")
-        
-            print("Miniconda not found. Installing Miniconda...")
-            subprocess.run(["start", "/wait", "Miniconda3-latest-Windows-x86_64.exe", "/InstallationType=JustMe", "/RegisterPython=0", "/AddToPath=1", "/S"], shell=True)
+    
+        print("Downloading the latest Miniconda installer...")
+        response = requests.get(installer_url, stream=True)
+        with open(installer_name, "wb") as file:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:  # Filter out keep-alive chunks
+                    file.write(chunk)
+            print("Download completed.")        
+    
+        print("Installing Miniconda...")   
+        subprocess.run(["start", "/wait", "Miniconda3-latest-Windows-x86_64.exe", "/InstallationType=JustMe", "/RegisterPython=0", "/AddToPath=1", "/S"], shell=True)
+        print("Miniconda Installed Successfully")
     else:
         print("Miniconda is already installed.")
 
